af7214f02bf1cc638228281e94a3426615eb330b
Alexander Koenigsberger <ak826@jfm.ece.cornell.edu>
2020-10-08 10:58:30 -0400

    separate run scripts

---
+++ run_lock_pbksb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -k eo
+#PBS -m abe
+#PBS -M ak826@cornell.edu
+#PBS -N lock_pbksb_crc235_ak826
+/home/crc235/ece5750_projects/ece5750/lock_cv_pbksb $n $p
---
+++ run_pbksb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -k eo
+#PBS -m abe
+#PBS -M ak826@cornell.edu
+#PBS -N pbksb_crc235_ak826
+/home/crc235/ece5750_projects/ece5750/pbksb $n $p
