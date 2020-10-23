2d11740f5f50192e2e1843b81446c4f16872f66d
Claire Rebecca Caplan <crc235@jfm.ece.cornell.edu>
2020-10-22 20:11:10 -0400

    last project stuff

--- run_pbksb.sh
+++ run_pbksb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -k eo
 #PBS -m abe
-#PBS -M ak826@cornell.edu
-#PBS -N pbksb_crc235_ak826
+#PBS -M crc235@cornell.edu
+#PBS -N pbksb
 /home/crc235/ece5750_projects/ece5750/pbksb $n $p
